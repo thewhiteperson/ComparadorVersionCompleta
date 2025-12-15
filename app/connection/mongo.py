e44ff0fc279a1be4e460914d7a7b72f66b80e67b
Jhonatan Blanco <jhonatanblanco01@hotmail.com>
2025-12-14 22:19:00 -0500

    Se lanza version con requirements

--- app/connection/mongo.py
+++ app/connection/mongo.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB = os.getenv("MONGO_DB")
+
+if not MONGO_URI or not MONGO_DB:
+    raise Exception("Variables de entorno de MongoDB no configuradas")
 
 client = MongoClient(MONGO_URI)
 db = client[MONGO_DB]
